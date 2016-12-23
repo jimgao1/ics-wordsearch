ab2f29f38598b3137143dbb3ff886a494edbbae0
349122844 <349122844@303R0224HX19DZ1.EDU.YRDSB.NET>
2016-12-23 09:59:11 -0500

    Joey

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,23 @@ import java.util.ArrayList;
 import java.util.Scanner;//Import all the libraries required 
 public class Main extends JFrame implements ActionListener
 {
+	JFrame gridFrame=new JFrame();
+	JFrame ioFrame=new JFrame();
 	JLabel wLabel[]=new JLabel[2];
 	JButton opButton[]=new JButton[3];
 	JPanel generalPan=new JPanel();
 	JPanel buttonPan=new JPanel();
+	JPanel xyPan=new JPanel();
+	JTextField xField=new JTextField(5);
+	JTextField yField=new JTextField(5);
+	JTextField inField= new JTextField(20);
+	JTextField outField= new JTextField(20);
+	JLabel inLabel= new JLabel("Enter the input file name");
+	JLabel outLabel= new JLabel("Enter the soluiton file name");
+	JPanel inPan = new JPanel();
+	JPanel outPan = new JPanel();
 	public Main()
-	{
-		setTitle("Word Puzzle");//Set the title of the frame
-        setSize(450,130);//Set the size of the frame
-        setResizable(false);//Size of the title is unchangeable
-		
+	{		
 		wLabel[0]=new JLabel("Welcome to the word puzzle generator and solver");
 		wLabel[1]=new JLabel("Please select if you want to generate a puzzle or solve a puzzle");
 		opButton[0]=new JButton("Generate");
@@ -26,18 +33,45 @@ public class Main extends JFrame implements ActionListener
 		opButton[2].addActionListener(this);
 		
 		BoxLayout generalLayout	=new BoxLayout(generalPan,BoxLayout.Y_AXIS);//Set the general layout
-		FlowLayout layoutB = new FlowLayout();
-        buttonPan.setLayout(layoutB);//Set the layout for the Button Panel
+		
+		gridFrame.setTitle("Word Puzzle Program");//Set the title of the frame
+        gridFrame.setSize(450,130);//Set the size of the frame
+        gridFrame.setResizable(false);//Size of the title is unchangeable
         
+        buttonPan.setLayout(new FlowLayout());//Set the layout for the Button Panel
         buttonPan.add(opButton[0]);
         buttonPan.add(opButton[1]);
         buttonPan.add(opButton[2]);
+        generalPan.setLayout(new FlowLayout());
         generalPan.add(wLabel[0]);
         generalPan.add(wLabel[1]);
         generalPan.add(buttonPan);
+        xyPan.setLayout(new FlowLayout());
+        xyPan.add(xField);
+        xyPan.add(yField);
         
-        add(generalPan);//Add general panel to the frame
-        setVisible(true);//Set visibility to true
+        gridFrame.setLayout(new GridLayout(1, 3));
+        gridFrame.add(generalPan);//Add general panel to the frame
+        gridFrame.add(buttonPan);
+        gridFrame.add(xyPan);
+        gridFrame.setVisible(true);//Set visibility to true
+        
+        ioFrame.setTitle("Input and Output Files");
+        ioFrame.setSize(450,130);
+        ioFrame.setResizable(false);
+        ioFrame.setLocation(450,0);
+        
+        inPan.setLayout(new FlowLayout());
+        inPan.add(inLabel);
+        inPan.add(inField);
+        outPan.setLayout(new FlowLayout());
+        outPan.add(outLabel);
+        outPan.add(outLabel);
+        
+        ioFrame.setLayout(new GridLayout(1, 2));
+        ioFrame.add(inPan);
+        ioFrame.add(outPan);
+        ioFrame.setVisible(true);
 	}
 	public void actionPerformed(ActionEvent event)//Action Perform Method
     {
@@ -63,8 +97,6 @@ public class Main extends JFrame implements ActionListener
 	public static void main(String args[])
 	{
 		Main mainframe=new Main();
-		/*
-		 * //I made some changes
 		ArrayList<String> words = new ArrayList<>();
 		words.add("sample_word");
 		
@@ -77,13 +109,12 @@ public class Main extends JFrame implements ActionListener
 		//The i'th index in the results refers to the result of the i'th word in the word list
 		//The element at index i is null if that word cannot be found in the grid
 		
-		/*
-		 * 	Each of the results contain 3 pieces of information:
+		 /* 	Each of the results contain 3 pieces of information:
 		 * 		- The X coord of the first letter
 		 * 		- The Y coord of the first letter
 		 * 		- The direction of the word (from 0 to 7)
 		 * 
-		 * 	For example, below is the result for the first word
+		 * 	For example, below is the result for the first word */
 		 
 		
 		if (results[0] != null){
@@ -95,6 +126,5 @@ public class Main extends JFrame implements ActionListener
 		} else {
 			//The word is not found
 		}
-		*/
 	}
 }
